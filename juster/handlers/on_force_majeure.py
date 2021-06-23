c84b5d0dfa1210be4b5eeec64ef1f2a2ed54b201
Lev Gorodetskiy <github@droserasprout.space>
2021-06-23 09:49:10 +0300

    Majeure, fix harbinger

---
+++ juster/handlers/on_force_majeure.py
@@ -0,0 +1,27 @@
+from juster.utils import get_event
+from typing import Optional
+
+from dipdup.models import OperationData, Origination, Transaction
+from dipdup.context import HandlerContext
+from juster.utils import from_mutez
+import juster.models as models
+
+from juster.types.juster.parameter.trigger_force_majeure import TriggerForceMajeureParameter
+from juster.types.juster.storage import JusterStorage
+
+
+
+async def on_force_majeure(
+    ctx: HandlerContext,
+    trigger_force_majeure: Transaction[TriggerForceMajeureParameter, JusterStorage],
+    fee_tx: OperationData,
+) -> None:
+    event_id, event_diff = get_event(trigger_force_majeure.storage)
+
+    event = await models.Event.filter(id=event_id).get()
+    event.status = models.EventStatus.CANCELED
+    await event.save()
+
+    fee_collector, _ = await models.User.get_or_create(address=fee_tx.target_address)
+    fee_collector.total_fees_collected += from_mutez(fee_tx.amount)  # type: ignore
+    await fee_collector.save()
--- juster/handlers/on_oracle_update.py
+++ juster/handlers/on_oracle_update.py
@@ -11,16 +11,15 @@ from juster.types.harbinger_normalizer.big_map.asset_map_value import AssetMapVa
 
 async def on_oracle_update(
     ctx: HandlerContext,
-    asset_map: List[BigMapDiff[AssetMapKey, AssetMapValue]],
+    asset_map: BigMapDiff[AssetMapKey, AssetMapValue],
 ) -> None:
-    for asset in asset_map:
-        if not asset.value:
-            continue
-        symbol = asset.key.__root__
-        currency_pair, _ = await models.CurrencyPair.get_or_create(symbol=symbol)
-        quote = models.Quote(
-            currency_pair=currency_pair,
-            price=asset.value.computedPrice,
-            timestamp=asset.value.lastUpdateTime,
-        )
-        await quote.save()
+    if not asset_map.value:
+        return
+    symbol = asset_map.key.__root__
+    currency_pair, _ = await models.CurrencyPair.get_or_create(symbol=symbol)
+    quote = models.Quote(
+        currency_pair=currency_pair,
+        price=asset_map.value.computedPrice,
+        timestamp=asset_map.value.lastUpdateTime,
+    )
+    await quote.save()
