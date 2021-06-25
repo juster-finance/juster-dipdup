1aec6e12d54b2253046cfd72c4556e5866105c84
Lev Gorodetskiy <github@droserasprout.space>
2021-06-25 15:40:48 +0300

    Optional fee_tx

--- juster/handlers/on_withdraw.py
+++ juster/handlers/on_withdraw.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from dipdup.models import OperationData, Transaction
 from dipdup.context import HandlerContext
 
@@ -13,6 +14,7 @@ async def on_withdraw(
     ctx: HandlerContext,
     withdraw: Transaction[WithdrawParameter, JusterStorage],
     withdraw_tx: OperationData,
+    fee_tx: Optional[OperationData] = None,
 ) -> None:
     event_id = int(withdraw.parameter.eventId)
     assert withdraw_tx.amount
@@ -33,3 +35,8 @@ async def on_withdraw(
         user=user,
         amount=amount
     ).save()
+
+    if fee_tx:
+        fee_collector, _ = await models.User.get_or_create(address=fee_tx.target_address)
+        fee_collector.total_fees_collected += from_mutez(fee_tx.amount)  # type: ignore
+        await fee_collector.save()
