99fbe380972eb27ce4aac57a40ae456188b050e0
ztepler <ztepler@gmail.com>
2022-04-19 00:42:36 +0300

    implemented on_deposit_liquidity handler

--- juster/handlers/on_deposit_liquidity.py
+++ juster/handlers/on_deposit_liquidity.py
@@ -4,8 +4,27 @@ from juster.types.pool.storage import PoolStorage
 from dipdup.models import Transaction
 from juster.types.pool.parameter.deposit_liquidity import DepositLiquidityParameter
 
+import juster.models as models
+from juster.utils import parse_datetime
+from juster.utils import from_mutez
+
+
 async def on_deposit_liquidity(
     ctx: HandlerContext,
     deposit_liquidity: Transaction[DepositLiquidityParameter, PoolStorage],
 ) -> None:
-    ...
+    user, _ = await models.User.get_or_create(address=deposit_liquidity.data.sender_address)
+    assert deposit_liquidity.data.amount
+    assert len(deposit_liquidity.storage.entries) == 1
+    entry_id = int(next(iter(deposit_liquidity.storage.entries)))
+    entry_diff = deposit_liquidity.storage.entries[str(entry_id)]
+    amount = from_mutez(deposit_liquidity.data.amount)
+
+    entry = models.EntryLiquidity(
+        id=entry_id,
+        user=user,
+        accept_time=parse_datetime(entry_diff.acceptAfter),
+        amount=amount
+    )
+    await entry.save()
+
