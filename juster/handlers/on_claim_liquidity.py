2699d81fec1639df4182c1c5484880aba59fd816
ztepler <ztepler@gmail.com>
2022-04-19 02:27:21 +0300

    added on_claim_liquidity handler [WIP]

--- juster/handlers/on_claim_liquidity.py
+++ juster/handlers/on_claim_liquidity.py
@@ -4,8 +4,26 @@ from juster.types.pool.storage import PoolStorage
 from dipdup.models import Transaction
 from juster.types.pool.parameter.claim_liquidity import ClaimLiquidityParameter
 
+import juster.models as models
+from juster.utils import (
+    get_position,
+    get_entry,
+    process_pool_shares
+)
+
+
 async def on_claim_liquidity(
     ctx: HandlerContext,
     claim_liquidity: Transaction[ClaimLiquidityParameter, PoolStorage],
 ) -> None:
-    ...
+    position_id, position_diff = get_position(claim_liquidity.storage)
+    position = await models.PoolPosition.filter(id=position_id).get()
+    new_shares = process_pool_shares(position_diff.shares)
+    param_shares = process_pool_shares(claim_liquidity.parameter.shares)
+    claimed_shares = position.shares - new_shares
+    assert claimed_shares == param_shares
+
+    position.shares -= claimed_shares
+    assert position.shares >= 0
+    await position.save()
+
