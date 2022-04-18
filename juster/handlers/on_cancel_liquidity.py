431548391b68c060b132e91704d7855973034cfd
ztepler <ztepler@gmail.com>
2022-04-19 02:03:41 +0300

    implemented on_cancel_liquidity handler

--- juster/handlers/on_cancel_liquidity.py
+++ juster/handlers/on_cancel_liquidity.py
@@ -4,8 +4,16 @@ from juster.types.pool.parameter.cancel_liquidity import CancelLiquidityParamete
 from dipdup.models import Transaction
 from juster.types.pool.storage import PoolStorage
 
+import juster.models as models
+
+
 async def on_cancel_liquidity(
     ctx: HandlerContext,
     cancel_liquidity: Transaction[CancelLiquidityParameter, PoolStorage],
 ) -> None:
-    ...
+    # TODO: is it OK to access this __root__ or there are any other ways? [2]
+    entry_id = int(cancel_liquidity.parameter.__root__)
+    entry = await models.EntryLiquidity.filter(id=entry_id).get()
+    entry.status = models.EntryStatus.CANCELED
+    await entry.save()
+
