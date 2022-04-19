afcf4868b46379f656740c2c3b733b3262c6693b
ztepler <ztepler@gmail.com>
2022-04-19 04:47:46 +0300

    implemented on_pay_reward handler

--- juster/handlers/on_pay_reward.py
+++ juster/handlers/on_pay_reward.py
@@ -4,8 +4,24 @@ from dipdup.context import HandlerContext
 from juster.types.pool.storage import PoolStorage
 from juster.types.pool.parameter.pay_reward import PayRewardParameter
 
+import juster.models as models
+from juster.utils import (
+    get_pool_event,
+    from_mutez
+)
+
+
 async def on_pay_reward(
     ctx: HandlerContext,
     pay_reward: Transaction[PayRewardParameter, PoolStorage],
 ) -> None:
-    ...
+    pool_event_id, pool_event_diff = get_pool_event(pay_reward.storage)
+    # TODO: is it OK to access this __root__ or there are any other ways? [3]
+    param_event_id = int(pay_reward.parameter.__root__)
+    assert pool_event_id == param_event_id
+    amount = from_mutez(pay_reward.data.amount)
+
+    event = await models.PoolEvent.filter(id=pool_event_id).get()
+    event.result = amount
+    await event.save()
+
