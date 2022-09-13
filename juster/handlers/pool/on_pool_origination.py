a451ec37ecfc5b37e0198ffd487a58abbbee79ac
ztepler <ztepler@gmail.com>
2022-09-13 18:59:07 +0300

    filtering pool contracts by sender address

--- juster/handlers/pool/on_pool_origination.py
+++ juster/handlers/pool/on_pool_origination.py
@@ -8,32 +8,36 @@ async def on_pool_origination(
     pool_origination: Origination[PoolStorage],
 ) -> None:
 
-    # TODO: validate that pool should be added (by creator address? by contract code?)
     # TODO: making name from lines params // metadata?
     contract_address = pool_origination.data.originated_contract_address
     creator = pool_origination.data.sender_address
     manager = pool_origination.storage.manager
-    # TODO: validate code?
+    approved_source = ctx.template_values['approved_source']
 
-    # TODO: ctx.logger.info
-    await ctx.add_contract(
-        name=f'pool_{contract_address}',
-        address=contract_address,
-        typename='pool'
-    )
-    # TODO: juster_core added in add_line process...
-    # !? bound to ctx.template_values?
-    # - bound to add_line?
-    # - do not bound to juster_core at all?
-    # - other ways?
+    is_approved = approved_source == creator == manager
 
-    await ctx.add_index(
-        name=f'pool_{contract_address}',
-        template='pool',
-        values={
-            'datasource': ctx.template_values['datasource'],
-            'contract': f'pool_{contract_address}',
-            'juster_core': ctx.template_values['juster_core'],
-        },
-    )
+    if is_approved:
+        await ctx.add_contract(
+            name=f'pool_{contract_address}',
+            address=contract_address,
+            typename='pool'
+        )
+        # TODO: juster_core added in add_line process...
+        # !? bound to ctx.template_values?
+        # - bound to add_line?
+        # - do not bound to juster_core at all?
+        # - other ways?
+
+        await ctx.add_index(
+            name=f'pool_{contract_address}',
+            template='pool',
+            values={
+                'datasource': ctx.template_values['datasource'],
+                'contract': f'pool_{contract_address}',
+                'juster_core': ctx.template_values['juster_core'],
+            },
+        )
+
+    if not is_approved:
+        ctx.logger.info("not approved pool: {contract_address}")
 
