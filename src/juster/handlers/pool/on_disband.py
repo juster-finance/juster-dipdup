efe29937430d4b05b96988d7dba989cbbe7aebca
ztepler <ztepler@gmail.com>
2022-11-08 16:07:06 +0300

    implemented pool disband handler

--- src/juster/handlers/pool/on_disband.py
+++ src/juster/handlers/pool/on_disband.py
@@ -1,11 +1,25 @@
-
-from juster.types.pool.storage import PoolStorage
 from dipdup.context import HandlerContext
-from juster.types.pool.parameter.disband import DisbandParameter
 from dipdup.models import Transaction
+
+import juster.models as models
+from juster.types.pool.parameter.disband import DisbandParameter
+from juster.types.pool.storage import PoolStorage
+from juster.utils import update_pool_state
+
 
 async def on_disband(
     ctx: HandlerContext,
     disband: Transaction[DisbandParameter, PoolStorage],
 ) -> None:
-    ...
+
+    assert disband.storage.isDisbandAllow is True
+    pool_address = disband.data.target_address
+    pool = await models.Pool.get(address=pool_address)
+    pool.is_disband_allow = True
+    await pool.save()
+
+    await update_pool_state(
+        pool=pool,
+        action=models.PoolHistoryAction.POOL_DISBANDED,
+        data=disband.data,
+    )
